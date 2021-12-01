0f1ae8363053f36a245f2bbc14bb76bcfe8f61d2
Daphne Carvalho <dah.feh@hotmail.com>
2021-12-01 00:45:23 -0300

    Creating function count_elements of a list.

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count_elements(list, 0)
+
+  defp count_elements([], acc), do: acc
+
+  defp count_elements([_head | tail], acc) do
+    acc = acc + 1
+    count_elements(tail, acc)
   end
 end
